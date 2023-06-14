7de057621609e093f600f9ada96111296fe07da5
WonHyung Shin <wonhyung64@icloud.com>
2023-06-14 20:43:43 +0900

    feat - add set random seed function

--- module/utils.py
+++ module/utils.py
@@ -1,4 +1,6 @@
 import torch
+import random
+import numpy as np
 
 
 def check_data(name):
@@ -21,3 +23,16 @@ def check_device():
         device = "cpu"
     
     return device
+
+
+def set_seed(random_seed):
+    """
+    Set random seed.
+    """
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    random.seed(random_seed)
