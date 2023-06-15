41dbcb14f9df84483fe4f18cefa6778362d94e92
WonHyung Shin <wonhyung64@icloud.com>
2023-06-15 14:33:52 +0900

    feat - reconstruction

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from module.dataset.refining import (
 from module.utils import (
     check_data,
     check_device,
+    set_seed,
 )
 from module.dataset.loader import (
     IRDataset,
@@ -25,11 +26,12 @@ from module.dataset.loader import (
 #%%
 if __name__ == "__main__":
     data_root = "."
-    name = "Yahoo"
-    # name = "Web30K"
+    # name = "Yahoo"
+    name = "Web30K"
     # name = "Istella"
     data_dir = f"{data_root}/data/{name}"
-    batch_size=4
+    batch_size = 4
+    data_seed = 0
 
     #checking
     check_data(name)
@@ -45,15 +47,18 @@ if __name__ == "__main__":
         refine_raw_data(data_dir, datasets, feat_num)
 
     #dataloader
+    set_seed(data_seed)
     train_loader = data.DataLoader(
         dataset=IRDataset(data_dir, "train"), batch_size=batch_size, shuffle=True, collate_fn=ir_collate_fn)
     valid_loader = data.DataLoader(
         dataset=IRDataset(data_dir, "valid"), batch_size=batch_size, collate_fn=ir_collate_fn)
     test_loader = data.DataLoader(
         dataset=IRDataset(data_dir, "test"))
-    
+
 
     for batch_x, batch_y in train_loader:
+        batch_x = batch_x.to(device)
+        batch_y = batch_y.to(device)
         print(batch_x.shape, batch_y.shape)
         break
     for batch_x, batch_y in valid_loader:
@@ -62,6 +67,12 @@ if __name__ == "__main__":
     for batch_x, batch_y in test_loader:
         print(batch_x.shape, batch_y.shape)
         break
-    
 
 # %%
+
+true = batch_y
+pred = batch_y
+relevance = batch_y
+relevance.sum(axis=1)
+
+
