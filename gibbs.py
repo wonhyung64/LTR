209708d4a81f1a9fc01361d75826a2633b398f23
WonHyung Shin <wonhyung64@icloud.com>
2023-09-07 22:23:02 +0900

    feat - complete gibbs sampling

--- gibbs.py
+++ gibbs.py
@@ -1,108 +1,137 @@
 #%%
-import numpy as np
-
-
-#%%
-"""SET UP"""
-mu = np.array([1.,2.,3.,4.])
-
-diagonal = 4.
-off_diagonal = .25
-cov = np.ones([4, 4]) * off_diagonal
-np.fill_diagonal(cov, diagonal, wrap=True)
-
-pi_1 = np.array([4,3,2,1])
-pi = pi_1
-
-
-#%%
 import copy
-# item_indices = np.arange(0, len(mu))
-item_indices = np.argsort(mu)
-
-rb_sample_num = 1000
-gibbs_sample_num = 1000
-burnin_num = 500
-omega = np.linalg.inv(cov)
-
-"""try"""
-item_j = 0
-
-pi_sorted = copy.deepcopy(pi)
-pi_sorted.sort(order=)
-
-omega[item_j, ]
-np.argsort()
-type(item_indices)
-
-
-
+import numpy as np
 from typing import List 
+from scipy.stats import truncnorm
+
+
+def gibbs_sampling(
+    gibbs_sample_num: int,
+    burnin_num: int,
+    pi: np.ndarray,
+    z_vec: np.ndarray,
+    mu: np.ndarray,
+    omega: np.ndarray,
+    ) -> np.ndarray:
+
+    print("Burn In")
+    for _ in range(burnin_num):
+        z_vec_copy: np.ndarray = copy.deepcopy(z_vec)
+        z_vec= gibbs_1epoch(pi, z_vec_copy, mu, omega)
+
+    print("Gibbs Sampling")
+    gibbs_samples: List = []
+    for _ in range(gibbs_sample_num):
+        z_vec_copy: np.ndarray = copy.deepcopy(z_vec)
+        z_vec= gibbs_1epoch(pi, z_vec_copy, mu, omega)
+        gibbs_samples.append(z_vec)
+    gibbs_z: np.ndarray = np.array(gibbs_samples)
+
+    return gibbs_z
+
+
+def gibbs_1epoch(
+    pi: np.ndarray,
+    z_vec: np.ndarray,
+    mu: np.ndarray,
+    omega: np.ndarray
+    ) -> np.ndarray:
+
+    item_j: int
+    for item_j in pi:
+        lower_bound: float
+        upper_bound: float
+        lower_bound, upper_bound = compute_bound(z_vec, item_j)
+
+        cond_mu: float = compute_cond_mu(item_j, pi, z_vec, mu, omega)
+        cond_var: float = compute_cond_var(item_j, omega)
+
+        trn_arg_lower: float = (lower_bound - cond_mu) / cond_var
+        trn_arg_upper: float = (upper_bound - cond_mu) /cond_var
+
+        gibbs_sample: np.ndarray = truncnorm.rvs(trn_arg_lower, trn_arg_upper, size=1)
+        gibbs_z_j: float = gibbs_sample[0] * cond_var + cond_mu
+        z_vec[item_j] = gibbs_z_j
+
+    return z_vec
+
+
+def compute_bound(z_vec: np.ndarray, item_j: int) -> List[float]:
+    lower_bound_cond: np.ndarray = (z_vec - z_vec[item_j]) < 0
+    upper_bound_cond: np.ndarray = (z_vec - z_vec[item_j]) > 0
+
+    if np.any(lower_bound_cond):
+        lower_bound: float = np.max(z_vec[lower_bound_cond])
+    else:
+        lower_bound: float = -np.inf
+
+    if np.any(upper_bound_cond):
+        upper_bound: float = np.min(z_vec[upper_bound_cond])
+    else: 
+        upper_bound: float = np.inf
+
+    return lower_bound, upper_bound
 
 
 def compute_cond_mu(
     item_j: int,
-    item_indices: np.ndarray,
+    pi: np.ndarray,
+    z_vec: np.ndarray,
     mu: np.ndarray,
     omega: np.ndarray,
-    pi: np.ndarray
     ) -> float:
 
     sum_term: List[float] = []
     omega_jj: float = omega[item_j, item_j]
-    for item_k  in item_indices:
+    for item_k  in pi:
         if item_k == item_j:
             continue
         omega_jk: float = omega[item_j, item_k]
-        sum_term.append(omega_jk / omega_jj * (pi[item_k] - mu[item_k]))
-    cond_mu: float = mu[item_j] - np.sum(sum_term)
+        sum_term.append(omega_jk * (z_vec[item_k] - mu[item_k]))
+    cond_mu: float = mu[item_j] - np.sum(sum_term) / omega_jj
 
     return cond_mu
 
 
-def compute_cond_var(item_j: int, cov: np.ndarray) -> float:
-    return cov[item_j, item_j]
+def compute_cond_var(item_j: int, omega: np.ndarray) -> float:
+    return 1 / omega[item_j, item_j]
 
 
-def compute_trn_mu(
-    rb_sample_num: int,
-    cond_mu: float,
-    cond_cov: float
-    ) -> float:
-    cond_sample: np.ndarray = np.random.normal(cond_mu, cond_cov, rb_sample_num)
+#%%
+if __name__ == "__main__":
 
-    rb_mu: float = np.mean(cond_sample) 
+    """SET UP"""
+    seed = 0
+    gibbs_sample_num: int = 10000
+    burnin_num: int = 500
 
+    mu: np.ndarray = np.array([1.,2.,3.,4.])
+    diagonal: float = 4.
+    off_diagonal: float = .25
+    cov:np.ndarray = np.ones([4, 4]) * off_diagonal
+    np.fill_diagonal(cov, diagonal, wrap=True)
+    omega: np.ndarray = np.linalg.inv(cov)
 
-    return rb_mu
-
-
-cond_mu = compute_cond_mu(item_j, item_indices, mu, omega, pi)
-cond_var = compute_cond_var(item_j, cov)
-
-
-
-a, b = (myclip_a - cond_mu) / cond_var, (myclip_b - cond_mu) / cond_var
-item_j
-from scipy.stats import truncnorm
-item_j
-item_indices
-
-upper_bound_cond = (pi - pi[item_j]) > 0
-
-lower_bound_cond = (pi - pi[item_j]) < 0
-
-(pi - 4) < 0
-(pi - 4) < 0
-pi - 3
-
-def gibbs_sampling(gibbs_sample_num, burnin_num):
-    total_iter_num = burnin_num + gibbs_sample_num
-    pass
+    z_vec: np.ndarray = np.array([2., 1.5, 1., .5])
+    pi: np.ndarray = np.argsort(z_vec, )[::-1]
 
 
+    """EXECUTE GIBBS FOR ONE PI"""
+    np.random.seed(seed)
+    gibbs_z: np.ndarray = gibbs_sampling(
+        gibbs_sample_num,
+        burnin_num,
+        pi,
+        z_vec,
+        mu,
+        omega)
 
 
-
+    """SHOW RESULTS"""
+    print()
+    print(f"Mu_hat: \n{np.mean(gibbs_z, axis=0)}")
+    print()
+    print(f"Covariance_hat: \n{np.cov(gibbs_z.T)}")
 
 # %%
+
