10e7274ca3e1f5cdae99b3d0fafdf4184cea0b2c
WonHyung Shin <wonhyung64@icloud.com>
2023-11-17 14:48:22 +0900

    feat - add rankt5 encoder only model

--- rankt5.py
+++ rankt5.py
@@ -24,14 +24,31 @@ class RankT5(nn.Module):
         outputs = self.dense(decoder_hidden_states)
         return outputs
 
+
+class RankT5Enc(nn.Module):
+    def __init__(self, base_model="t5-small"): 
+        super(RankT5Enc,self).__init__()
+        self.base_model = T5ForConditionalGeneration.from_pretrained(base_model)
+        self.encoder = self.base_model.encoder
+        self.dense = nn.Linear(512,1) # load and initialize weights
+
+    def forward(self, input_ids):
+        _encoder_hidden_states = self.encoder(input_ids=input_ids)
+        encoder_hidden_states = _encoder_hidden_states.last_hidden_state
+        pool_states = torch.mean(encoder_hidden_states, dim=1)
+        outputs = self.dense(pool_states)
+        return outputs
+
+
+
 #%%
 vocab_size = 32_000
 input_sentence_size = None
+base_model="t5-small"
 dataset = datasets.load_dataset("ms_marco", "v1.1")
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
-help(tokenizer)
-model = RankT5()
-tokenizer.convert_tokens_to_ids("<extra_id_10>")
+
+tokenizer = T5Tokenizer.from_pretrained(base_model)
+model = RankT5Enc()
 
 i = 8
 sample = dataset["train"][i]
@@ -39,7 +56,7 @@ doc = sample["passages"]["passage_text"][0]
 query = sample["query"]
 x = f"Query: {query} Document: {doc}"
 input_ids = tokenizer(x, return_tensors="pt").input_ids
-input_ids >= 32000
 score = model(input_ids)
+score
 
 #%% MODEL
